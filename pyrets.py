cda6e4f3001a445980d3bcdd574accb8bd63a20f
Matthew Ryan Grace <matthew.r.grace@gmail.com>
2017-03-07 16:53:41 -0500

    use basic auth vs. digest, add another library for comparison.

--- pyrets.py
+++ pyrets.py
@@ -2,15 +2,15 @@
 
 import requests
 from xml.etree import ElementTree
-from urllib.parse import urlparse, urljoin
+from urlparse import urljoin
+from urlparse import urlparse
 import socket
 import hashlib
 import time
-import urllib.parse
 
 
 class RetsSession(object):
-    def __init__(self, user, passwd, user_agent, user_agent_passwd, rets_version, login_url):
+    def __init__(self, user, passwd, user_agent, user_agent_passwd, rets_version, login_url, proxies=None):
         self.rets_ua_authorization = None
         self.user = user
         self.passwd = passwd
@@ -19,32 +19,34 @@ class RetsSession(object):
         self.rets_version = rets_version
         self.base_url = self._get_base_url(login_url)
         self.login_url = login_url
+        self.proxies = proxies
         self._session = None
         self.login_called = False
 
     def login(self):
         self._session = requests.session()
-        
+
         headers = {'User-Agent':self.user_agent,
                'RETS-Version':self.rets_version,
                'Accept':"*/*"}
-        
+
         if self.user_agent_passwd:
             headers['RETS-UA-Authorization'] = self._calculate_rets_ua_authorization(''
                                                                             , self.user_agent
                                                                             , self.user_agent_passwd
                                                                             , self.rets_version)
-            
-        auth = requests.auth.HTTPDigestAuth(self.user, self.passwd)
-        
+
+        #auth = requests.auth.HTTPDigestAuth(self.user, self.passwd)
+        auth = requests.auth.HTTPBasicAuth(self.user, self.passwd)
+
         self._session.headers = headers
         self._session.auth = auth
 
-        login_result = self._session.get(self.login_url)
+        login_result = self._session.get(self.login_url, proxies=self.proxies)
         login_result.raise_for_status()
-        
+
         self.server_info = self._parse_login_response(login_result.text)
-        
+
         if self.user_agent_passwd:
             self.rets_ua_authorization = self._calculate_rets_ua_authorization(login_result.cookies['RETS-Session-ID']
                                                                             , self.user_agent
@@ -56,18 +58,18 @@ class RetsSession(object):
     def logout(self):
         if not self.login_called:
             raise NoLoginException("You need to call login before logout")
-        
+
         logout_url = urljoin(self.base_url, self.server_info['Logout'])
         if self.user_agent_passwd:
             self._set_rets_ua_authorization()
-        logout_response = self._session.get(logout_url)
+        logout_response = self._session.get(logout_url, proxies=self.proxies)
         logout_response.raise_for_status()
         return logout_response.text
 
     def getobject(self, obj_type, resource , obj_id):
         if not self.login_called:
             raise NoLoginException("You need to call login before getobject")
-        
+
         for i in range(3):
             try:
                 return self._getobject(obj_type, resource , obj_id)
@@ -77,23 +79,23 @@ class RetsSession(object):
                     time.sleep(5)
                 else:
                     raise
-                
+
     def getmetadata(self):
         if not self.login_called:
             raise NoLoginException("You need to call login before getmetadata")
-        
+
         get_meta_url = urljoin(self.base_url, self.server_info['GetMetadata'])
         if self.user_agent_passwd:
             self._set_rets_ua_authorization()
-        response = self._session.get(get_meta_url + '?Type=METADATA-SYSTEM&ID=*&Format=STANDARD-XML')
+        response = self._session.get(get_meta_url + '?Type=METADATA-SYSTEM&ID=*&Format=STANDARD-XML', proxies=self.proxies)
         response.raise_for_status()
         self._parse_getmetadata_response(response.text)
         return response.text
-    
+
     def search(self, resource, search_class, query, limit, select):
         if not self.login_called:
             raise NoLoginException("You need to call login before search")
-        
+
         if limit:
             limit = 'NONE'
         params = {'SearchType': resource,
@@ -108,11 +110,11 @@ class RetsSession(object):
         search_url = urljoin(self.base_url, self.server_info['Search'])
         if self.user_agent_passwd:
             self._set_rets_ua_authorization()
-        search_response = self._session.post(search_url, params)
+        search_response = self._session.post(search_url, params, proxies=self.proxies)
         search_response.raise_for_status()
         self._parse_search_response(search_response.text)
         return search_response.text
-         
+
     def _get_base_url(self, url_str):
         url_parts = urlparse(url_str)
         resURL = url_parts.scheme + "://" + url_parts.netloc
@@ -122,7 +124,7 @@ class RetsSession(object):
         getobject_url = urljoin(self.base_url, self.server_info['GetObject'])
         if self.user_agent_passwd:
             self._set_rets_ua_authorization()
-        getobject_response = self._session.get(getobject_url + "?Type=%s&Resource=%s&ID=%s" % (obj_type, resource, obj_id))
+        getobject_response = self._session.get(getobject_url + "?Type=%s&Resource=%s&ID=%s" % (obj_type, resource, obj_id), proxies=self.proxies)
         getobject_response.raise_for_status()
         if getobject_response.headers['content-type'] == 'text/plain':
             self._parse_getobject_response(getobject_response.text)
@@ -132,7 +134,7 @@ class RetsSession(object):
         reply_code, reply_text = self._get_code_text(login_resp)
         if reply_code != '0':
             raise LoginException(reply_code + "," + reply_text)
-        
+
         login_xml = ElementTree.fromstring(login_resp)
         if len(login_xml) > 0:
             rets_info = login_xml[0].text.split('\n')
@@ -145,30 +147,30 @@ class RetsSession(object):
                 key_value_pair = info_item.split('=')
                 rets_info_dict[key_value_pair[0].strip()] = key_value_pair[1].strip()
         return rets_info_dict
-    
+
     def _parse_getobject_response(self, response):
         reply_code, reply_text = self._get_code_text(response)
         if reply_code != '0':
             raise GetObjectException(reply_code + "," + reply_text)
-    
+
     def _parse_search_response(self, response):
         if not response:
             raise SearchException('Empty response')
         reply_code, reply_text = self._get_code_text(response)
         if reply_code not in ['0']:
-            raise SearchException(reply_code + "," + reply_text) 
-        
+            raise SearchException(reply_code + "," + reply_text)
+
     def _parse_getmetadata_response(self, response):
         reply_code, reply_text = self._get_code_text(response)
         if reply_code != '0':
-            raise GetMetadataException(reply_code + "," + reply_text) 
-        
-    def _get_code_text(self, response_xml): 
+            raise GetMetadataException(reply_code + "," + reply_text)
+
+    def _get_code_text(self, response_xml):
         xml_obj = ElementTree.fromstring(response_xml)
         reply_code = xml_obj.attrib['ReplyCode']
         reply_text = xml_obj.attrib['ReplyText']
         return reply_code, reply_text
-        
+
     def _set_rets_ua_authorization(self):
         self._session.headers['RETS-UA-Authorization'] = self.rets_ua_authorization;
 
@@ -179,9 +181,9 @@ class RetsSession(object):
         retssessionid = sid
         digestHash = hashlib.md5(bytes(a1hashed + ':' + retsrequestid + ':' + retssessionid + ':' + rets_version, 'utf-8')).hexdigest()
         return 'Digest ' + digestHash
-    
+
 class LoginException(Exception):
-    pass   
+    pass
 
 class GetObjectException(Exception):
     pass
@@ -194,7 +196,3 @@ class GetMetadataException(Exception):
 
 class NoLoginException(Exception):
     pass
-
-
-    
-
